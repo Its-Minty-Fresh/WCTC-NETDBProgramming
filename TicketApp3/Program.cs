af466d23a5ca93ef811e1d2ffb451f1cc6c23e71
Matthew Kapitan <mkapitan@my.wctc.edu>
2020-02-25 19:36:53 -0600

    tryi again

---
+++ TicketApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("    What would you like to do?\n\n" +
+                              "    1) View Tickets\n" +
+                              "    2) View Enhancements\n" +
+                              "    3) View Tasks\n" +
+                              "    4) Quit");
+
+            string resp = Console.ReadLine();
+            if (resp == "1")
+            {
+                //show tickets
+            }
+            else if (resp == "2")
+            {
+                //add ticket
+            }
+        }
+    }
+}
